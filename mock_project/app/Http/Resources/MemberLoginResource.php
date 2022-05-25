6a8db993bac3560263913aa39d5cb420e4bd99ce
Nguyen Quang Dong <dongnq@vnext.vn>
2022-05-25 23:14:17 +0700

    authen

---
+++ mock_project/app/Http/Resources/MemberLoginResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MemberLoginResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'member_code'=> $this->member_code,
+            'full_name' => $this->full_name,
+            'email' => $this->email,
+            'avatar' => $this->avatar,
+            'avatar_official' => $this->avatar_official,
+        ];
+    }
+}
