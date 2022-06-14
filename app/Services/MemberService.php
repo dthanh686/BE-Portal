dfe9c7c70374e1427c3a490e3122f3a8f4735771
namnamb1 <vinaphone232@gmail.com>
2022-06-14 09:23:00 +0700

    edit path avatar

--- app/Services/MemberService.php
+++ app/Services/MemberService.php
@@ -21,13 +21,13 @@ class MemberService extends BaseService
         if ($data->hasFile('avatar')) {
             $newFileName = uniqid() . '-' . $data->avatar->getClientOriginalName();
             $imagePath = $data->avatar->storeAs(config('common.default_image_path') . 'members', $newFileName);
-            $member->avatar = 'http://127.0.0.1:8000/storage' . str_replace('public', '', $imagePath);
+            $member->avatar = 'http://54.179.42.101/storage' . str_replace('public', '', $imagePath);
         }
 
         if ($data->hasFile('avatar_official')) {
             $newFileName = uniqid() . '-' . $data->avatar_official->getClientOriginalName();
             $imagePath = $data->avatar_official->storeAs(config('common.default_image_path') . 'members', $newFileName);
-            $member->avatar_official = 'http://127.0.0.1:8000/storage' . str_replace('public', '', $imagePath);
+            $member->avatar_official = 'http://54.179.42.101/storage' . str_replace('public', '', $imagePath);
         }
 
         return $member->save();
